87b961a2f957cb1e0e32ad75ce1f0368369867e1
Hamed <ostovari.eng@gmail.com>
2026-03-21 12:04:08 +0100

    Removed unused imports and codes

--- src/app/(shared-layout)/dashboard/my-posts/create/page.tsx
+++ src/app/(shared-layout)/dashboard/my-posts/create/page.tsx
@@ -1,64 +1,9 @@
 "use client";
 import { createBlogAction } from "@/src/actions";
-import { Button } from "@/src/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/src/components/ui/card";
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-} from "@/src/components/ui/field";
-import { Checkbox } from "@/src/components/ui/checkbox";
-import { Input } from "@/src/components/ui/input";
-import { Textarea } from "@/src/components/ui/textarea";
-import { Loader2 } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useActionState, useEffect, useState } from "react";
-import { toast } from "sonner";
+
 import { PostForm } from "../_components/PostForm";
 
-type InitialState = {
-  errors: Record<string, string>;
-  success: boolean;
-  data: Record<string, string | File | null | boolean>;
-};
-
-const initialState: InitialState = {
-  success: false,
-  errors: {},
-  data: {},
-};
 const CreatePostPage = () => {
-  const router = useRouter();
-  const [state, formAction, isPending] = useActionState(
-    createBlogAction,
-    initialState,
-  );
-
-  const [draftChecked, setDraftChecked] = useState(
-    state?.data?.status === true,
-  );
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
-
-  useEffect(() => {
-    if (!state) return;
-
-    if (state.success) {
-      toast.success("Post created!");
-      router.push("/");
-      return;
-    }
-    if (state.data.status) {
-      setDraftChecked(!!state.data.status);
-    }
-    if (state.errors?.general) toast.error(state.errors.general);
-  }, [state, router]);
   return (
     <div className="py-4">
       <div className="text-center mb-12">
